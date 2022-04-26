58c67994a204bba83fd07681fe5a0bd4fcc57487
AhmedAmrAwadallahK8 <AwadallahAhmed25@gmail.com>
2022-04-25 18:09:46 -0700

    Added new files

---
+++ td_math.c
@@ -0,0 +1,17 @@
+#include "td_math.h"
+
+//Returns the max between two numbers
+int max(int x, int y){
+    if(x > y){
+        return x;
+    }
+    return y;
+}
+
+//Returns the min between two numbers
+int min(int x, int y){
+    if(x < y){
+        return x;
+    }
+    return y;
+}
---
+++ td_math.h
@@ -0,0 +1,11 @@
+#ifndef TD_MATH_H
+#define TD_MATH_H
+
+
+//Returns the max between two numbers
+int max(int x, int y);
+
+//Returns the min between two numbers
+int min(int x, int y);
+
+#endif
